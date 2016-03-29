770cce7e570f1c0afb77b4ae891ec078b7175b65
Carlos Guzmán Camus <cguzmancamus@gmail.com>
2016-03-29 11:36:13 -0300

    Ejemplo python talle

---
+++ Ejemplo_Python/firmador.py
@@ -0,0 +1,53 @@
+# -*- coding: utf-8 -*-
+import requests
+# Importamos variales parametrizadas
+from config import files, variables_generales, firmar
+import jwt
+
+token = jwt.encode({'expiration': firmar.expiration, 'rut': firmar.rut, 'proposito': firmar.proposito, 'entidad': firmar.entidad},
+                   variables_generales.SECRET,
+                   algorithm='HS256')
+
+####### Armamos Body
+archivo1 = {}
+archivo2 = {}
+data = {}
+
+data['token'] = token
+data['api_token_key'] = variables_generales.APP_CODE
+data['files'] = []
+
+# Armamos json de archivo 1
+archivo1['type'] = files.type_file1
+archivo1['content'] = files.file1_base64
+archivo1['checksum'] = files.checksum1_sha256
+archivo1['description'] = files.description1
+archivo1['layout'] = files.layout
+
+# Armamos json de archivo 2
+archivo2['type'] = files.type_file2
+archivo2['content'] = files.file2_base64
+archivo2['description'] = files.description2
+archivo2['checksum'] = files.checksum2_sha256
+#archivo2['layout'] = files.layout
+
+# Archivo 1 y 2 en una lista []
+data['files'].append(archivo1)
+data['files'].append(archivo2)
+
+# Subimos archivos y Obtenemos session
+headers={"Content-Type": "application/json; charset=UTF-8", 'User-agent': 'Mozilla/5.0'}
+import pdb;pdb.set_trace()
+r = requests.post(variables_generales.URL, json=data, headers=headers)
+try:
+    session_token =  str(r.json()['session_token'])
+except:
+    print r.text
+    exit()
+
+# Firmamos Archivos subidos
+headers={"Content-Type": "application/json; charset=UTF-8",
+         "OTP": firmar.otp,
+         'User-agent': 'Mozilla/5.0'}
+r = requests.get(variables_generales.URL + "/" + session_token, headers=headers)
+print r.text
